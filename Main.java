6721a8687cafa62bfd87fce45c9e46e1a7a4846d
Pranav <pranav.techiegeek@gmail.com>
2023-08-18 14:43:36 +0530

    lab-3 ready

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 class Main {
     public static void main(String args[]) {
-        String str = "Hello, world!";
-        System.out.println(str.substring(0, 5));
+        int[] arr = new int[3];
+        System.out.println(arr[1]);
 }
